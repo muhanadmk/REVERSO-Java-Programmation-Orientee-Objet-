1e577ca5d29a612c00403886302929c90d287057
muhanadmk <mouhandmk1@gmail.com>
2022-01-20 11:47:26 +0100

    add configuration juint in pom.xml

---
+++ ReversoJava/src/test/java/fr/afpa/pompey/cda08/demo/TestClient.java
@@ -0,0 +1,16 @@
+package fr.afpa.pompey.cda08.demo;
+
+import fr.afpa.pompey.cda08.demo.com.company.metier.Client;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.JavaTimeConversionPattern;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class 
+TestClient extends Client {
+    public TestClient(){}
+
+}
