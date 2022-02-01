888f72f962d490e9076dac6f4817db1844b6a333
muhanadmk <mouhandmk1@gmail.com>
2022-02-01 11:52:04 +0100

    ajouter bouton et list pour afficher lite clients qui ont contrat

--- ReversoJava/src/main/java/fr/afpa/pompey/cda08/demo/com/company/vues/Accueil.java
+++ ReversoJava/src/main/java/fr/afpa/pompey/cda08/demo/com/company/vues/Accueil.java
@@ -1,23 +1,24 @@
+
 package fr.afpa.pompey.cda08.demo.com.company.vues;
 
-import fr.afpa.pompey.cda08.demo.com.company.DAO.ConnexionManager;
-import fr.afpa.pompey.cda08.demo.com.company.DAO.DaoClient;
-import fr.afpa.pompey.cda08.demo.com.company.DAO.DaoProspect;
-import fr.afpa.pompey.cda08.demo.com.company.DAO.DaoSqlEx;
+import fr.afpa.pompey.cda08.demo.com.company.DAO.*;
 import fr.afpa.pompey.cda08.demo.com.company.exception.metier.ExceptionMetier;
+import fr.afpa.pompey.cda08.demo.com.company.metier.Client;
+import fr.afpa.pompey.cda08.demo.com.company.metier.Contrat;
 import fr.afpa.pompey.cda08.demo.com.company.metier.Societe;
 import fr.afpa.pompey.cda08.demo.com.company.utile.ChoixUtilisateur;
 
 import javax.swing.*;
 import java.awt.event.*;
-import java.io.IOException;
 import java.util.ArrayList;
+
 
 /**
  * c'est la première page de l'application, la page d'accueil sur laquelle on montre pour les user
  * ce qu'il va choisir entre gestion de client ou prospect,
  * cela ca serait dans seul écran s'adapter à le choix de la user
  */
+
 public class Accueil extends JFrame {
 
     private JPanel contentPane;
@@ -33,24 +34,35 @@ public class Accueil extends JFrame {
     private JComboBox listAficha;
     private JButton sortirButton;
     private JButton homeButton;
-    private ArrayList <Societe> listDeObjectTeleharger = new ArrayList<>();
+    private JButton contartsButton;
+    private JComboBox ClinetsQuiOntContra;
+    private JButton afficherSonContrat;
+    private ArrayList<Societe> listDeObjectTeleharger = new ArrayList<>();
+    private ArrayList <Client> clientsContrat = new ArrayList<>();
     private boolean flageClient = false;
     private boolean condtionAficheEdit = false;
     private Accueil accueilFreme;
 
-    /**
+
+/**
      * constructeur classe d'accueil sur lequel on définit les mêmes on veut
      */
+
     public Accueil() {
         setContentPane(contentPane);
         //définir la taille de l'app
         setSize(800, 600);
         listAficha.setVisible(false);
         //hide cette section
+        afficherSonContrat.setVisible(false);
         manipulerSection.setVisible(false);
+        ClinetsQuiOntContra.setVisible(false);
+        contartsButton.setVisible(false);
+
 /**
  *  quand clice on affiche la gestion des clients et cascher prospects
  */
+
         gestionDesClientsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -59,14 +71,17 @@ public class Accueil extends JFrame {
                 flageClient = true;
                 choixclientsOuProspects = "Clients";
                 rempilerComboBox(true);
+                contartsButton.setVisible(true);
             }
         });
 
-        /**
+
+/**
          *  quand clice on affiche la gestion des  prospects et cascher  clients
          *  on envoie en tant que paramètre le choix le user si client si no le PageManipulerDeList
          *  il sait que ca un prospect
          */
+
         gestionDesProspectsButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -76,9 +91,11 @@ public class Accueil extends JFrame {
                 rempilerComboBox(false);
             }
         });
-        /**
+
+/**
          *  quand clice on retourne ver page de l'Accueil et bien afficher la gestion des  prospects et clients
          */
+
         homeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -87,16 +104,21 @@ public class Accueil extends JFrame {
                 gestionDesProspectsButton1.setVisible(true);
                 listAficha.removeAllItems();
                 listAficha.setVisible(false);
+                contartsButton.setVisible(false);
+                ClinetsQuiOntContra.removeAllItems();
+                ClinetsQuiOntContra.setVisible(false);
                 condtionAficheEdit = false;
             }
         });
-        /**
+
+/**
          * quand on clique on va ouvrir la Page Manipuler De List
          * et on catch s'il y a des erreurs
          * et on va directement sur la création
          *  on envoie en tant que paramètre le choix le user si client si no le PageManipulerDeList
          *          il sait que ca un prospect
          */
+
         creationButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -106,19 +128,21 @@ public class Accueil extends JFrame {
                             null);
                     pageManipulerDeList.setVisible(true);
                     dispose();
-                }catch (DaoSqlEx daoSqlEx){
+                } catch (DaoSqlEx daoSqlEx) {
                     daoSqlEx.printStackTrace();
                 }
 
             }
         });
-        /**
+
+/**
          * quand on clique on va ouvrir la Page Manipuler De List
          * et on catch s'il y a des erreurs
          * et on va directement sur la modifier et on peut modifier tout Suaf le id
          * on envoie en tant que paramètre le choix le user si client si no le PageManipulerDeList
          *         il sait que ca un prospect
          */
+
         modifier.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -145,31 +169,32 @@ public class Accueil extends JFrame {
                 }
             }
         });
-        /**
+
+/**
          * quand on clique on va ouvrir la Page Manipuler De List
          * et on catch s'il y a des erreurs
          * et on va directement sur la suprimer et on peut pas modifier , on peut que suprimer
          *  on envoie en tant que paramètre le choix le user si client si no le PageManipulerDeList
          *     il sait que ca un prospect
          */
+
         suprimer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 listAficha.setVisible(true);
                 try {
                     if (condtionAficheEdit) {
-                            try {
-                                Societe societe = listDeObjectTeleharger.get(listAficha.getSelectedIndex());
-                                PageManipulerDeList pageManipulerDeList = new PageManipulerDeList(flageClient,
-                                        ChoixUtilisateur.choix.SUPRIMER,
-                                        societe);
-                                pageManipulerDeList.setVisible(true);
-                                dispose();
-                            } catch (DaoSqlEx daoSqlEx) {
-                                daoSqlEx.printStackTrace();
-                            }
+                        try {
+                            Societe societe = listDeObjectTeleharger.get(listAficha.getSelectedIndex());
+                            PageManipulerDeList pageManipulerDeList = new PageManipulerDeList(flageClient,
+                                    ChoixUtilisateur.choix.SUPRIMER,
+                                    societe);
+                            pageManipulerDeList.setVisible(true);
+                            dispose();
+                        } catch (DaoSqlEx daoSqlEx) {
+                            daoSqlEx.printStackTrace();
                         }
-
+                    }
                     condtionAficheEdit = true;
                 } catch (IndexOutOfBoundsException errIndex) {
                     listAficha.removeAllItems();
@@ -179,30 +204,56 @@ public class Accueil extends JFrame {
                 }
             }
         });
-        /**
+
+/**
          * quand on clique on va ouvrir la Page affichagee
          * et on catch s'il y a des erreurs
          * et on va directement sur J table qui va affiche ce qui est dans le liste
          *  on envoie en tant que paramètre le choix le user si client si no le PageManipulerDeList
          *          il sait que ca un prospect
          */
+
         affichageeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    if (listAficha.getItemCount() != 0) {
-                        Affichage affichage = new Affichage(flageClient);
-                        affichage.setVisible(true);
-                        dispose();
-                    } else {
-                        messageErr("message Err !!", "vous avez pas " + choixclientsOuProspects + " " +
-                                "a les afficher");
-                    }
-                } catch (DaoSqlEx daoSqlEx) {
-                    daoSqlEx.printStackTrace();
+                if (listAficha.getItemCount() != 0) {
+                    Affichage affichage = new Affichage(flageClient);
+                    affichage.setVisible(true);
+                    dispose();
+                } else {
+                    messageErr("message Err !!", "vous avez pas " + choixclientsOuProspects + " " +
+                            "a les afficher");
                 }
             }
         });
+        contartsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ClinetsQuiOntContra.setVisible(true);
+                remplierClinetsQuiOntContrat();
+            }
+        });
+        ClinetsQuiOntContra.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (condtionAficheEdit){
+                    remplierClinetsQuiOntContrat();
+                    afficherSonContrat.setVisible(true);
+                }
+                condtionAficheEdit = true;
+            }
+        });
+        afficherSonContrat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Client client = clientsContrat.get(ClinetsQuiOntContra.getSelectedIndex());
+                System.out.println(client.getId());
+                ContratsVue contratsVue = new ContratsVue(client.getId());
+                contratsVue.setVisible(true);
+            }
+        });
+
         sortirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -211,11 +262,36 @@ public class Accueil extends JFrame {
         });
     }
 
-    /**
+    private ArrayList getListContrat() {
+        try {
+            return DaoContrat.findAllCilentQuiOntContrat(ConnexionManager.conn());
+        } catch (DaoSqlEx daoSqlEx) {
+            daoSqlEx.printStackTrace();
+            return null;
+        }catch (ExceptionMetier exceptionMetier) {
+            exceptionMetier.printStackTrace();
+            return null;
+        }
+    }
+
+    private void remplierClinetsQuiOntContrat() {
+        if (ClinetsQuiOntContra == null || ClinetsQuiOntContra.getItemCount() == 0) {
+            for (int i = 0; i < getListContrat().size(); i++) {
+                Client client = (Client) getListContrat().get(i);
+                ClinetsQuiOntContra.addItem(client.getSociale());
+                clientsContrat.add(client);
+            }
+        }
+
+    }
+
+
+/**
      * méthode pour récupérer l'erreur et l'afficher sur l'écran et après on retronne à la page d'accueil
      *
      * @param exception
      */
+
     private void retourAlacuile(ExceptionMetier exception) {
         messageErr("Msg Eerr", exception.getMessage());
         Accueil accueil = new Accueil();
@@ -223,7 +299,8 @@ public class Accueil extends JFrame {
         accueil.getListAficha().setVisible(false);
     }
 
-    /**
+
+/**
      * méthode pour récupérer l'erreur et l'afficher sur l'écran et après on retronne à la page d'accueil
      * sauf cette méthode on écrit le message de erreur
      *
@@ -246,6 +323,9 @@ public class Accueil extends JFrame {
         } catch (DaoSqlEx daoSqlEx) {
             daoSqlEx.printStackTrace();
             return null;
+        }catch (ExceptionMetier exceptionMetier){
+            exceptionMetier.printStackTrace();
+            return null;
         }
     }
 
@@ -263,3 +343,4 @@ public class Accueil extends JFrame {
         return listAficha;
     }
 }
+
