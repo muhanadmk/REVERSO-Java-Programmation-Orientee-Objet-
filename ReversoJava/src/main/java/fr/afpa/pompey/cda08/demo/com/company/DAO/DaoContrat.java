670dcf72b839118a7850612f011a27bc8d26fd6d
muhanadmk <mouhandmk1@gmail.com>
2022-02-01 11:50:04 +0100

    ajouter methode findAllCilentQuiOntContrat

--- ReversoJava/src/main/java/fr/afpa/pompey/cda08/demo/com/company/DAO/DaoContrat.java
+++ ReversoJava/src/main/java/fr/afpa/pompey/cda08/demo/com/company/DAO/DaoContrat.java
@@ -2,43 +2,62 @@ package fr.afpa.pompey.cda08.demo.com.company.DAO;
 
 
 import fr.afpa.pompey.cda08.demo.com.company.exception.metier.ExceptionMetier;
+import fr.afpa.pompey.cda08.demo.com.company.metier.Address;
+import fr.afpa.pompey.cda08.demo.com.company.metier.Client;
 import fr.afpa.pompey.cda08.demo.com.company.metier.Contrat;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class DaoContrat {
     private static PreparedStatement preparedStmt;
-    public DaoContrat(){}
-    public static ArrayList<Contrat> findByIdClient(Connection con, int Idcliente) throws DaoSqlEx {
+    private static Statement stmt;
+    public DaoContrat() {
+    }
+
+    public static ArrayList<Contrat> findByIdClient(Connection con, Integer idCilent) throws DaoSqlEx {
         ArrayList<Contrat> listContrat = new ArrayList<>();
         try {
-            String query = "SELECT * FROM `contrat` INNER JOIN clients ON contrat.idClient = clients.Id_cliente" +
-                " WHERE idClient=?";
-            preparedStmt = con.prepareStatement(query);
-            preparedStmt.setInt(1, Idcliente);
+            String query = "SELECT `id`, `idClient`, `libelleDeContrat`, `montantDeContrat` FROM `contrat`" +
+                        " INNER JOIN clients ON contrat.idClient = clients.Id_cliente WHERE  Id_cliente =?";
+                preparedStmt = con.prepareStatement(query);
+                preparedStmt.setInt(1, idCilent);
             ResultSet rs = preparedStmt.executeQuery();
             while (rs.next()) {
-            Contrat contrat= new Contrat(rs.getInt("id"), rs.getInt("idClient"),
-                    rs.getString("libelleDeContrat"),rs.getDouble("montantDeContrat"));
-                System.out.println(contrat);
-            listContrat.add(contrat);
-        }
-    }  catch (SQLException e ) {
-        throw new DaoSqlEx("error base de données vous pouvez pas recuperer les contrats essaiez ultiareemnt");
-    } finally {
-        if (preparedStmt!= null) {
-            try {
-                preparedStmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw new DaoSqlEx("error base de données essaiez ultiareemnt");
+                Contrat contrat = new Contrat(rs.getInt("id"), rs.getInt("idClient"),
+                        rs.getString("libelleDeContrat"), rs.getDouble("montantDeContrat"));
+                listContrat.add(contrat);
             }
+            if (preparedStmt != null) {
+                preparedStmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DaoSqlEx("error base de données vous pouvez pas recuperer " +
+                    "les contrats essaiez ultiareemnt");
         }
-    }
         return listContrat;
+    }
+    public static ArrayList<Client> findAllCilentQuiOntContrat(Connection con) throws DaoSqlEx, ExceptionMetier {
+        String query = "SELECT DISTINCT name_Client, Id_cliente  FROM `contrat` " +
+                "INNER JOIN clients ON contrat.idClient = clients.Id_cliente";
+        ArrayList listClientQuiontdeContrat = new ArrayList();
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+               Client client = new Client();
+               client.setSociale(rs.getString("name_Client"));
+               client.setId(rs.getInt("Id_cliente"));
+                listClientQuiontdeContrat.add(client);
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DaoSqlEx("err Basee de donnees ,vous n'avez pas reussi a recuperer les Cilents");
+        }
+        return listClientQuiontdeContrat;
     }
 }
